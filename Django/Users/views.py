c854e216b7031c9c20301315af9dd411e3a3f2d1
Dark <mohamed2003elashmawy@gmail.com>
2023-05-21 16:38:42 +0300

    testing new webhook

--- Django/Users/views.py
+++ Django/Users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from Management.models import Employee
 
 # Create your views here.
 
@@ -9,7 +10,17 @@ def createEmployee(request):
       form = UserCreationForm(request.POST)
       if form.is_valid():
            name = form.cleaned_data.get('name')
-           messages.success(request, 'Employee created successfully')
+           id = form.cleaned_data.get('id')
+           email = form.cleaned_data.get('email')
+           address = form.cleaned_data.get('address')
+           number = form.cleaned_data.get('number')
+           gender = form.cleaned_data.get('gender')
+           dob = form.cleaned_data.get('dob')
+           status = form.cleaned_data.get('status')
+           vacation = form.cleaned_data.get('vacation')
+           salary = form.cleaned_data.get('salary')
+           Employee.objects.create(id = id, name = name, email = email, address = address, mobile = number, gender = gender, status = status, 
+                                   availableVacation = vacation, approvedVacation = 0, dob = dob, salary = salary)
            return redirect('hr-home')
       else:
            messages.ERROR(request, 'Error')
