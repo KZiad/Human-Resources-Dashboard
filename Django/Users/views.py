bea7ea22f03d839847e4f28175d1adb4fd995de0
Dark <mohamed2003elashmawy@gmail.com>
2023-05-21 18:24:41 +0300

    finally got the form working and saving data in db

--- Django/Users/views.py
+++ Django/Users/views.py
@@ -7,27 +7,20 @@ from Management.models import Employee
 
 def createEmployee(request):
     if request.method == 'POST':
-      form = UserCreationForm(request.POST)
-      if form.is_valid():
-           name = form.cleaned_data.get('name')
-           id = form.cleaned_data.get('id')
-           email = form.cleaned_data.get('email')
-           address = form.cleaned_data.get('address')
-           number = form.cleaned_data.get('number')
-           gender = form.cleaned_data.get('gender')
-           dob = form.cleaned_data.get('dob')
-           status = form.cleaned_data.get('status')
-           vacation = form.cleaned_data.get('vacation')
-           salary = form.cleaned_data.get('salary')
-           Employee.objects.create(id = id, name = name, email = email, address = address, mobile = number, gender = gender, status = status, 
-                                   availableVacation = vacation, approvedVacation = 0, dob = dob, salary = salary)
+           form_name = request.POST.get('name')
+           form_id = request.POST.get('id')
+           form_email = request.POST.get('email')
+           form_address = request.POST.get('address')
+           form_number = request.POST.get('number')
+           form_gender = request.POST.get('gender')
+           form_dob = request.POST.get('dob')
+           form_status = request.POST.get('status')
+           form_vacation = request.POST.get('vacation')
+           form_salary = request.POST.get('salary')
+           Employee.objects.create(id = form_id, name = form_name, email = form_email, address = form_address, mobile = form_number, gender = form_gender,
+                                    status = form_status, availableVacation = form_vacation, approvedVacation = 0, dob = form_dob, salary = form_salary)
            return redirect('hr-home')
-      else:
-           messages.ERROR(request, 'Error')
-      
-    else:
-        form = UserCreationForm()
-    return render(request, 'Users/addEmployees.html',{'form': form})
+    return render(request, 'Users/addEmployees.html')
 
 def update(request):
         return render(request, 'Users/update.html')
