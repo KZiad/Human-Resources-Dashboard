1e31fd140c4da8a60f353125bed6c3c5042eb8a0
dept60 <karsonziad@gmail.com>
2023-05-22 21:02:17 +0300

    Added vacation.html, still form

--- Django/Management/models.py
+++ Django/Management/models.py
@@ -19,3 +19,18 @@ class Employee(models.Model):
     # This is to be able to read the employee name and ID in admin menu
     def __str__(self):
         return f'{self.name} -- {self.id}'
+
+class Vacation(models.Model):
+    id = models.AutoField(primary_key = True)
+    employeeID = models.ForeignKey(Employee, on_delete = models.CASCADE)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    status = models.CharField(max_length = 12)
+    reason = models.CharField(max_length = 1500)
+    dateCreated = models.DateField(default= datetime.date.today, null= True)
+
+    # This is to be able to read the employee name and ID in admin menu
+    def __str__(self):
+        # Format example: (Vac. 1) Emp 1 | 2020-01-01 -> 2020-01-02 | Pending
+        return f'(Vac. {self.id}) Emp. {self.employeeID} | {self.startDate} -> {self.endDate} | {self.status}'
+        
--- Django/Users/views.py
+++ Django/Users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from Management.models import Employee
-
+from Management.models import Employee, Vacation
+import datetime
 # Create your views here.
 
 def createEmployee(request):
@@ -30,8 +30,36 @@ def update(request):
 def data(request):
         return render(request, 'Users/employeeData.html', {'empData': Employee.objects.all()})
 
-def vacation(request):
-        return render(request, 'Management/vacation.html')
+def vacation(request, id):
+        if (request.method == 'POST'):
+                form_data = request.POST
+                print(form_data)
+                form_id = form_data['empID']
+                format = '%Y-%m-%d'
+                form_beginDate = datetime.datetime.strptime(form_data['beginDate'], format) 
+                form_endDate = datetime.datetime.strptime(form_data['endDate'], format)
+                #the number of days between the start and end date
+                numDays = form_endDate - form_beginDate
+                numDays = numDays.days
+                print(numDays)
+                form_reason = form_data['reason']
+                # get the employee's available vacation days
+                # if the employee has enough days, then create the vacation request
+                # else, return an error message
+                
+                availableVacation = Employee.objects.filter(id = form_id).values('availableVacation')
+                print(availableVacation)
+                availableVacation = availableVacation[0]['availableVacation']
+                approvedVacation = Employee.objects.filter(id = form_id).values('approvedVacation')
+                approvedVacation = approvedVacation[0]['approvedVacation']
+                if (availableVacation < numDays):
+                        return render(request, 'Management/vacation.html', {'error': 'Not enough vacation days'})
+                Employee.objects.filter(id = form_id).update(availableVacation = availableVacation - numDays)
+                Employee.objects.filter(id = form_id).update(approvedVacation = approvedVacation + numDays)
+                VacEmployee = Employee.objects.get(id = form_id)
+                Vacation.objects.create(employeeID = VacEmployee, startDate = form_beginDate, endDate = form_endDate, status = 'Pending', reason = form_reason)
+                return redirect('hr-vacationList')
+        return render(request, 'Users/vacation.html')
 
 def vacationList(request):
         return render(request, 'Management/vacationList.html')
