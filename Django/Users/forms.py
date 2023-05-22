334ef5bcf2740e440f9c042613acfea28e8adc17
dept60 <karsonziad@gmail.com>
2023-05-22 23:02:25 +0300

    fixing things

--- Django/Users/forms.py
+++ Django/Users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from Management.models import Employee
+from Management.models import Employee, Vacation
 
 
 gender = (("Male", "Male"), ("Female", "Female"))
@@ -13,7 +13,7 @@ class EmployeeRegistrationForm(forms.ModelForm):
     address = forms.CharField(label= 'address', max_length = 100, error_messages = {"required": "Please enter an address"}, widget=forms.TextInput(attrs={'placeholder': 'Address'}))
     number = forms.CharField(label= 'number', max_length = 11, error_messages = {"required": "Please enter a phone number"}, widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
     gender = forms.ChoiceField(choices= gender, error_messages = {"required": "Please specify your gender"})
-    dob = forms.DateField(label= 'dob' ,error_messages = {"required": "Please enter your date of birth"}, widget=forms.TextInput(attrs={'placeholder': ' Year-Month-Day'}))
+    dob = forms.DateField(label= 'dob' ,error_messages = {"required": "Please enter your date of birth"}, widget = forms.NumberInput(attrs={'type':'date'}))
     status = forms.ChoiceField(choices= status, error_messages = {"required": "Please specify your status"})
     availableVacation = forms.IntegerField(label= 'availableVacation', error_messages = {"required": "Please enter the available vacation days"}, widget=forms.TextInput(attrs={'placeholder': 'Available Vacation Days'}))
     salary = forms.IntegerField(label= 'salary', error_messages = {"required": "Please enter the salary"}, widget=forms.TextInput(attrs={'placeholder': 'Salary'}))
@@ -40,3 +40,18 @@ class EmployeeUpdateForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = ['name', 'id', 'email', 'address', 'number', 'gender', 'dob', 'status', 'availableVacation', 'salary']
+
+
+class EmployeeVacationForm(forms.ModelForm):
+    # Auto filled fields
+    employeeID = forms.IntegerField(label= 'id', disabled= True)
+    EmployeeName = forms.CharField(label = 'name',max_length = 100, disabled= True)
+    
+    # User input fields
+    beginDate = forms.DateField(label= 'beginDate' ,error_messages = {"required": "Please enter the begin date"}, widget=forms.TextInput(attrs={'placeholder': ' Year-Month-Day'}))
+    endDate = forms.DateField(label= 'endDate' ,error_messages = {"required": "Please enter the end date"}, widget=forms.TextInput(attrs={'placeholder': ' Year-Month-Day'}))
+    reason = forms.CharField(label= 'reason', max_length = 1000, error_messages = {"required": "Please enter the reason"}, widget=forms.TextInput(attrs={'placeholder': 'Reason'}))
+    
+    class Meta:
+        model = Vacation
+        fields = ['employeeID', 'beginDate', 'endDate', 'status', 'reason']
--- Django/Users/views.py
+++ Django/Users/views.py
@@ -2,22 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from Management.models import Employee, Vacation
 import datetime
-from .forms import EmployeeRegistrationForm, EmployeeUpdateForm
+from .forms import *
 
 # Create your views here.
 
 def createEmployee(request):
-    if request.method == 'POST':
-        form = EmployeeRegistrationForm(request.POST)
+        if request.method == 'POST':
+                form = EmployeeRegistrationForm(request.POST)
 
-        if form.is_valid():
-                number = form.cleaned_data.get('number') 
-                if len(number) != 11:
-                        messages.error(request, "Please enter a valid phone number")
+                if form.is_valid():
+                        number = form.cleaned_data.get('number') 
+                        if len(number) != 11:
+                                messages.error(request, "Please enter a valid phone number")
+                                return render(request, 'Users/addEmployees.html', {'form': form})
+                        form.save()
+                        messages.success(request, "Employee added successfully")
+                        return redirect('hr-home')
+                else:
+                        messages.error(request, "Error please check your date input")
                         return render(request, 'Users/addEmployees.html', {'form': form})
-                form.save()
-                messages.success(request, "Employee added successfully")
-                return redirect('hr-home')
         else:
                 form = EmployeeRegistrationForm()
 
@@ -34,6 +37,9 @@ def update(request, id):
                         form.save()
                         messages.info(request, "Employee updated successfully")
                         return redirect('hr-home')
+                else:
+                        messages.error(request, "Error please check your date input")
+                        return render(request, 'Users/addEmployees.html', {'form': form})
         else:
                 form = EmployeeUpdateForm(instance=Employee.objects.get(id=id))
 
@@ -48,6 +54,28 @@ def data(request, id):
         return render(request, 'Users/employeeData.html', {'empData': Employee.objects.get(id=id)})
 
 def vacation(request, id):
+        if request.method == 'POST':
+                form = EmployeeVacationForm(request.POST, instance=Employee.objects.get(id=id))
+                if form.is_valid():
+                        # Check if the employee has enough vacation days
+                        # If they do, then create the vacation request
+                        # Else, return an error message
+                        beginDate = form.cleaned_data.get('startDate')
+                        endDate = form.cleaned_data.get('endDate')
+                        numDaysDelta = endDate - beginDate
+                        numDays = numDaysDelta.days
+                        availableVacation = Employee.objects.filter(id = id).values('availableVacation')
+                        availableVacation = availableVacation[0]['availableVacation']
+                        approvedVacation = Employee.objects.filter(id = id).values('approvedVacation')
+                        approvedVacation = approvedVacation[0]['approvedVacation']
+                        if (availableVacation < numDays):
+                                return render(request, 'Management/vacation.html', {'error': 'Not enough vacation days'})
+                        form.save()
+                        
+        else:
+                form = EmployeeUpdateForm(instance=Employee.objects.get(id=id))
+
+        return render(request, 'Users/update.html', {'form': form, 'id': id})
         if (request.method == 'POST'):
                 form_data = request.POST
                 print(form_data)
@@ -56,9 +84,8 @@ def vacation(request, id):
                 form_beginDate = datetime.datetime.strptime(form_data['beginDate'], format) 
                 form_endDate = datetime.datetime.strptime(form_data['endDate'], format)
                 #the number of days between the start and end date
-                numDays = form_endDate - form_beginDate
-                numDays = numDays.days
-                print(numDays)
+                numDaysDelta = form_endDate - form_beginDate
+                numDays = numDaysDelta.days
                 form_reason = form_data['reason']
                 # get the employee's available vacation days
                 # if the employee has enough days, then create the vacation request
